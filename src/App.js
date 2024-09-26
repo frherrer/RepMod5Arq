42ead2d86d2a22c2805e7fc61a0ef8dedf48afd4
Francisco Herrera <fjhrycstg@gmail.com>
2024-09-26 18:07:51 -0300

    prueba 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V3</h1>
+        <h1>Esta es una prueba</h1>
       </header>
     </div>
   );
